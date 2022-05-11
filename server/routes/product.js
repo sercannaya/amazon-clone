9d8569263b6aeb809e6758c0d73e4aaf93570693
Sercan NAYA <sercannayaa@gmail.com>
2022-05-11 14:38:52 +0300

    feat: photo upload middleware aws s3

--- server/routes/product.js
+++ server/routes/product.js
@@ -1,13 +1,15 @@
 const router = require('express').Router();
 const Product = require('../models/product');
 
+const upload = require('../middlewares/upload-photo');
+
 // POST request - create a new product
-router.post("/products", async (req, res) => {
+router.post("/products", upload.single("photo"), async (req, res) => {
   try {
     let product = new Product();
     product.title = req.body.title;
     product.description = req.body.description;
-    product.photo = req.body.photo
+    product.photo = req.file.location;
     product.stockQuantity = req.body.stockQuantity
 
     await product.save() // async
