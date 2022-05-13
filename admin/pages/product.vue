64e2f389583c34d320b1586a18ad1fb50bba2d52
Sercan NAYA <sercannayaa@gmail.com>
2022-05-13 22:13:27 +0300

    feat: Connected to API on product page categories and owners section

--- admin/pages/product.vue
+++ admin/pages/product.vue
@@ -12,16 +12,22 @@
               <div class="a-spacing-top-medium">
                 <label>Category</label>
                 <select class="a-select-option">
-                  <option value="1">1</option>
-                  <option value="2">2</option>
+                  <option 
+                    v-for="category in categories" 
+                    :value="category._id"
+                    :key="category._id"                    
+                  >{{ category.type }}</option>
                 </select>
               </div>
               <!-- Owner Dropdown -->
               <div class="a-spacing-top-medium">
                 <label>Owner</label>
                 <select class="a-select-option">
-                  <option value="1">1</option>
-                  <option value="2">2</option>
+                  <option 
+                    v-for="owner in owners" 
+                    :value="owner._id"
+                    :key="owner._id"                    
+                  >{{ owner.name }}</option>
                 </select>
               </div>
               <!-- Title Input -->
@@ -73,6 +79,26 @@
 
 <script>
 export default {
-
-}
+  async asyncData({ $axios }) {
+    try {
+      let categories = $axios.$get("http://localhost:3000/api/categories");
+      let owners = $axios.$get("http://localhost:3000/api/owners");
+  
+      const [catResponse, ownerResponse] = await Promise.all([
+        categories,
+        owners
+      ]);
+  
+      console.log(catResponse);
+  
+      return {
+        categories: catResponse.categories,
+        owners: ownerResponse.owners
+      };
+      
+    } catch (error) {
+      console.log(err);
+    }
+  }
+};
 </script>
