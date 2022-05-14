ec42d4b50ba0440ba57ca82d0c36acbfd27d2849
Sercan NAYA <sercannayaa@gmail.com>
2022-05-14 15:28:01 +0300

    feat: Client folder created

---
+++ client/pages/index.vue
@@ -0,0 +1,11 @@
+<template>
+  <div>
+    <nuxt-link to="/products" class="a-button-buy-again">Add a new product</nuxt-link>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'IndexPage'
+}
+</script>
