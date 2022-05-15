e885bec16a11c33bf1c210ec4c161cacceae888f
Sercan NAYA <sercannayaa@gmail.com>
2022-05-15 17:45:48 +0300

    feat: Added NavBar and Footer

--- client/pages/index.vue
+++ client/pages/index.vue
@@ -1,11 +1,33 @@
 <template>
   <div>
+    <NavBar />
+    <main>
+      <div class="listingPage">
+        <div class="container-fluid">
+          <div class="row">
+            <div class="col-xl-2 col-lg-3 md-4 col-sm-4">
+              <!-- Side Bar -->
+            </div>
+            <!-- Main Content -->
+            <div class="col-xl-10 col-lg-9 md-8 col-sm-8">
+              <FeaturedProduct />
+            </div>
+          </div>
+        </div>
+      </div>
+    </main>
     <nuxt-link to="/products" class="a-button-buy-again">Add a new product</nuxt-link>
+    <Footer />>
   </div>
 </template>
 
 <script>
+import NavBar from "~/components/NavBar.vue";
+import Footer from "../components/Footer.vue";
+import FeaturedProduct from "~/components/FeaturedProduct.vue";
+
 export default {
-  name: 'IndexPage'
+    name: "IndexPage",
+    components: { NavBar, Footer, FeaturedProduct }
 }
 </script>
