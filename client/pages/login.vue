b7d39146561686089a8739ad99c93f3bf3b618bd
Sercan NAYA <sercannayaa@gmail.com>
2022-05-23 20:06:26 +0300

    feat: Added login page

--- client/pages/login.vue
+++ client/pages/login.vue
@@ -11,17 +11,7 @@
           <form class="mt-4">
             <div class="a-box a-spacing-extra-large">
               <div class="a-box-inner">
-                <h1 class="a-spacing-small">Create account</h1>
-                <!-- Your Name -->
-                <div class="a-row a-spacing-base">
-                  <label for="ap_customer_name" class="a-form-label">Your name</label>
-                  <input 
-                    type="text" 
-                    id="ap_customer_name" 
-                    class="a-input-text form-control auth-autofocus auth-required-field auth-contact-verification-request-info"
-                    v-model="name"
-                  />
-                </div>
+                <h1 class="a-spacing-small">Sign in</h1>
                 <!-- Email -->
                 <div class="a-row a-spacing-base">
                   <label for="ap_customer_name" class="a-form-label">Email</label>
@@ -50,7 +40,7 @@
                 <div class="a-row a-spacing-extra-large mb-4">
                   <span class="a-button-primary">
                     <span class="a-button-inner">
-                      <span class="a-button-text" @click="onSignup">Create your Amazon account</span>
+                      <span class="a-button-text" @click="onLogin">Continue</span>
                     </span>
                   </span>
                   <div class="a-row a-spacing-top-medium a-size-small">
@@ -64,8 +54,8 @@
                 <hr />
                 <div class="a-row">
                   <b>
-                    Already have an account?
-                    <nuxt-link to="/login" class="a-link-emphasis">Sign in</nuxt-link>
+                    Don't have an account?
+                    <nuxt-link to="/signup" class="a-link-emphasis">Sign up</nuxt-link>
                   </b>
                 </div>
               </div>
@@ -84,24 +74,13 @@ export default {
   layout: none,
   data() {
     return {
-      name: "",
       email: "",
       password: "",
     }
   },
   methods: {
-    async onSignup() {
+    async onLogin() {
       try {
-        let data = {
-          name: this.name,
-          email: this.email,
-          password: this.password
-        }
-
-        let response = await this.$axios.$post("/api/auth/signup", data);
-        console.log(response);
-
-        if (response.success) {
           this.$auth.loginWith("local", {
             data: {
               email: this.email,
@@ -110,7 +89,6 @@ export default {
           });
         
         this.$router.push("/");
-        }
       } catch (err) {
         console.log(err)
       }
